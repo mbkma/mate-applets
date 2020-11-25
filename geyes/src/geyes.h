6700c34372c182a049b7d1d24a9481f2cd506fb6
rbuj <robert.buj@gmail.com>
2020-11-25 20:56:34 +0100

    geyes: do not use dirent.h nor PATH_MAX

--- geyes/src/geyes.h
+++ geyes/src/geyes.h
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 1999 Dave Camp <dave@davec.dhs.org>
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
- *  
+ *
  */
 
 #ifndef __GEYES_H__
@@ -28,58 +28,56 @@
 #include <mate-panel-applet.h>
 
 #define MAX_EYES 1000
+#define GEYES_SETTINGS_SCHEMA "org.mate.panel.applet.geyes"
+#define GEYES_SETTINGS_THEME_PATH_KEY "theme-path"
+
 typedef struct
 {
-	GtkWidget *pbox;
-    
-	gint selected_row;
+        GtkWidget       *pbox;
+        gint             selected_row;
 } EyesPropertyBox;
 
-typedef struct 
+typedef struct
 {
-	/* Applet */
-	MatePanelApplet *applet;
-	GtkWidget   *vbox;
-	GtkWidget   *hbox;
-	GtkWidget   **eyes;
-	guint        timeout_id;
-	gint 	    *pointer_last_x;
-	gint 	    *pointer_last_y;
+        /* Applet */
+        MatePanelApplet *applet;
+        GtkWidget       *vbox;
+        GtkWidget       *hbox;
+        GtkWidget      **eyes;
+        guint            timeout_id;
+        gint            *pointer_last_x;
+        gint            *pointer_last_y;
 
-	/* Theme */
-	GdkPixbuf *eye_image;
-	GdkPixbuf *pupil_image;
-	gchar *theme_dir;
-	gchar *theme_name;
-	gchar *eye_filename;
-	gchar *pupil_filename;
-	gint num_eyes;
-	gint eye_height;
-	gint eye_width;
-	gint pupil_height;
-	gint pupil_width;
-	gint wall_thickness;
+        /* Theme */
+        GdkPixbuf       *eye_image;
+        GdkPixbuf       *pupil_image;
+        gchar           *theme_dir;
+        gchar           *theme_name;
+        gchar           *eye_filename;
+        gchar           *pupil_filename;
+        gint             num_eyes;
+        gint             eye_height;
+        gint             eye_width;
+        gint             pupil_height;
+        gint             pupil_width;
+        gint             wall_thickness;
 
-	/* Properties */
-	EyesPropertyBox prop_box;
+        /* Properties */
+        EyesPropertyBox  prop_box;
 
-	/* Settings */
-	GSettings *settings;
+        /* Settings */
+        GSettings       *settings;
 } EyesApplet;
 
 /* eyes.c */
-void setup_eyes   (EyesApplet *eyes_applet);
-
-void destroy_eyes (EyesApplet *eyes_applet);
-
+void       setup_eyes         (EyesApplet   *eyes_applet);
+void       destroy_eyes       (EyesApplet   *eyes_applet);
 
 /* theme.c */
-void theme_dirs_create (void);
+void       theme_dirs_create  (void);
+gboolean   load_theme         (EyesApplet   *eyes_applet,
+                               const gchar  *theme_dir);
+void       properties_cb      (GtkAction    *action,
+                               EyesApplet   *eyes_applet);
 
-int load_theme    (EyesApplet        *eyes_applet,
-		   const gchar       *theme_dir);
-
-void properties_cb (GtkAction         *action,
-		    EyesApplet        *eyes_applet);
-
-#endif
+#endif /* __GEYES_H__ */
